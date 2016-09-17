8a74c13aa81b86927eaeb890d587ac828c4b7b6a
Ring <226214208@qq.com>
2016-09-18 01:04:30 +0800

    add form

--- mysite/static_file/dashboard.js
+++ mysite/static_file/dashboard.js
@@ -1,22 +1,22 @@
-$(function() {
+$(function () {
     $.ajax({
         type: 'GET',
         url: '/api/get_strategy_list',
         dataType: 'json',
-        success: function(data) {
+        success: function (data) {
             var strategy_list = data["strategy_list"];
             $("#strategy_list_info").html("");
             for (var i = 0; i < strategy_list.length; i++) {
                 var htmlStr = "<tr stock_id='" + strategy_list[i].id + "'>";
                 htmlStr += "<td>" + strategy_list[i].code + "</td>"
                 htmlStr += "<td>" + strategy_list[i].objective + "</td>"
-                htmlStr += "<td>" + strategy_list[i].return+"</td>"
+                htmlStr += "<td>" + strategy_list[i].return + "</td>"
                 htmlStr += "<td>" + strategy_list[i].risk + "</td>"
                 htmlStr += "<td>" + strategy_list[i].value_at_risk + "</td>"
                 htmlStr += "</tr>"
                 $("#strategy_list_info").append(htmlStr)
             }
-            $("#strategy_list_info tr").on('click', function() {
+            $("#strategy_list_info tr").on('click', function () {
                 window.location = "/strategy_list/" + $(this).attr("stock_id");
             });
         }
@@ -42,13 +42,20 @@ $(function() {
         var htmlStr = "<tr stock_id='" + strategy_list[i].id + "'>";
         htmlStr += "<td>" + strategy_list[i].code + "</td>"
         htmlStr += "<td>" + strategy_list[i].objective + "</td>"
-        htmlStr += "<td>" + strategy_list[i].return+"</td>"
+        htmlStr += "<td>" + strategy_list[i].return + "</td>"
         htmlStr += "<td>" + strategy_list[i].risk + "</td>"
         htmlStr += "<td>" + strategy_list[i].value_at_risk + "</td>"
         htmlStr += "</tr>"
         $("#strategy_list_info").append(htmlStr)
     }
-    $("#strategy_list_info tr").on('click', function() {
+    $("#strategy_list_info tr").on('click', function () {
         window.location = "/stock_list/" + $(this).attr("stock_id");
     });
+
+    for (var i = 20; i <= 85; i++) {
+        $("#user_age_select").append("<option value='" + i + "'>" + i + "</option>")
+    }
+    for (var i = 20; i <= 85; i++) {
+        $("#retire_age_select").append("<option value='" + i + "'>" + i + "</option>")
+    }
 })
