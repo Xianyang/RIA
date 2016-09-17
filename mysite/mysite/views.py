8d58ee3ce69fe0777e641238d7b2f3db5c9d22a1
Ring <226214208@qq.com>
2016-09-17 22:16:18 +0800

    update model detail page

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -16,15 +16,24 @@ def model(request,model_id):
     cursor = cnx.cursor(dictionary=True)
     query = "SELECT * FROM model_stocks WHERE model_id = %s" 
     cursor.execute(query, (model_id,))
+    model_stock_list =[]
+    for model_stock in cursor:
+        model_stock_list.append(model_stock)
+    id_str = "("+",".join([str(model_stock["stock_id"]) for model_stock in model_stock_list]) +")"
+    query = "SELECT * FROM fintech.stocks WHERE id IN %s"
+    cursor.execute(query, (model_id,))
     stock_list =[]
     for stock in cursor:
-        stock_list.append(stock)
+        for model_stock in model_stock_list:
+            if stock["id"] == model_stock["stock_id"]:
+                stock["weight"] = model_stock["weighting"]
+            stock_list.append(stock)
     query = "SELECT * FROM fintech.models WHERE id=%s"
     cursor.execute(query, (model_id,))
     model = {}
     for m in cursor:
         model = m
-    
+
     cursor.close()
     cnx.close()
     context = {'stock_list': stock_list,"model":model}
